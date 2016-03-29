4d86e6f9577444c4760d9fc07de40f8d0227d12e
oxylius <noah.hendrikx@gmail.com>
2016-03-29 16:00:49 +0200

    UserRegisterController Fix

---
+++ WebApplication1/ts/controller/UserRegisterController.ts
@@ -0,0 +1,14 @@
+﻿export interface IUserRegisterScope extends angular.IScope{
+    hello: String
+}
+
+export class UserRegisterController {
+
+    public static $inject = [
+        '$scope'
+    ];
+
+    constructor($scope: IUserRegisterScope) {
+        $scope.hello = 'salüü';
+    }
+}
