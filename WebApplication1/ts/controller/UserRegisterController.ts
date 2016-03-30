8d0b8ce1e1703d5ffecf2da2d20157876000e1f6
Pascal Marty <p-marty@sunrise.ch>
2016-03-30 11:33:43 +0200

    user-register view elements added fixed index.html height

--- WebApplication1/ts/controller/UserRegisterController.ts
+++ WebApplication1/ts/controller/UserRegisterController.ts
@@ -1,5 +1,7 @@
 ﻿export interface IUserRegisterScope extends angular.IScope{
-    hello: String
+    hello: String,
+    states: Object,
+    kanton: String;
 }
 
 export class UserRegisterController {
@@ -10,5 +12,10 @@ export class UserRegisterController {
 
     constructor($scope: IUserRegisterScope) {
         $scope.hello = 'salüü';
+        $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+            'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+            'WY').split(' ').map(function (state) {
+                return { abbrev: state };
+            })
     }
 }
