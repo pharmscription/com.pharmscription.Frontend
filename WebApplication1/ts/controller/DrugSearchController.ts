43f653ab7049e0264fe990e04a8bc5cb3e08ccc1
Pascal Marty <p-marty@sunrise.ch>
2016-04-06 21:40:58 +0200

    drug-search list added

--- WebApplication1/ts/controller/DrugSearchController.ts
+++ WebApplication1/ts/controller/DrugSearchController.ts
@@ -9,7 +9,13 @@ export class DrugSearchController {
     ];
 
     constructor(private drugRepository: DrugRepository) {
-        this.drugs = [];
+        this.drugs = new Array<Drug>();
+        for (var i = 0; i < 100; i++){
+            this.drugs[i] = new Drug();
+            this.drugs[i].composition = "composition";
+            this.drugs[i].drugDescription = "description" + i;
+        }
+        //this.drugs = [];
     }
 
     getDrugs(drugs: String): void {
@@ -20,4 +26,15 @@ export class DrugSearchController {
             this.drugs = [];
         });
     }
+
+    this.doSecondaryAction = function (event) {
+        $mdDialog.show(
+            $mdDialog.alert()
+                .title('Secondary Action')
+                .textContent('Secondary actions can be used for one click actions')
+                .ariaLabel('Secondary click demo')
+                .ok('Neat!')
+                .targetEvent(event)
+        );
+    };
 }
