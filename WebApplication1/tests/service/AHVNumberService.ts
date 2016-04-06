245d36c687d12f7154313e7d251e23bab3b6b974
Pascal Marty <p-marty@sunrise.ch>
2016-04-06 14:07:03 +0200

    AHVNumberService Test added (not tested)

---
+++ WebApplication1/tests/service/AHVNumberService.ts
@@ -0,0 +1,18 @@
+﻿///<reference path="../../tools/typings/main.d.ts"/>
+import {AHVNumberService} from "../../ts/service/AHVNumberService"
+
+describe("AHVNumberService Test", () => {
+    var ahvNumberService:AHVNumberService;
+
+    it("is correctly defined", () => {
+        expect(ahvNumberService).not.toBeUndefined();
+        expect(ahvNumberService).not.toBeNull();
+    });
+    it("has a default value if no value has been set", () => {
+        expect(ahvNumberService).toBe('');
+    });
+    it("correctly sets a value", () => {
+        ahvNumberService.setAHVNumber("111.1111.1111.11");
+        expect(ahvNumberService.getAHVNumber()).toBe("111.1111.1111.11");
+    });
+});
