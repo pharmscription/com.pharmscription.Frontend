4ced6531390f14c00ab9f72dee936bb44d7a9c70
oxylius <noah.hendrikx@gmail.com>
2016-04-11 11:21:55 +0200

    Drug Backend Mock implemented

--- WebApplication1/ts/appDev.ts
+++ WebApplication1/ts/appDev.ts
@@ -1,6 +1,8 @@
-﻿import Patient from './model/patient';
-import 'angular-mocks'
+﻿import 'angular-mocks'
 import app from './app'
+import Papa = require('papaparse');
+import Patient from 'ts/model/patient';
+import Drug from "ts/model/drug";
 
 export class AppDev {
     app: ng.IModule;
@@ -12,14 +14,38 @@ export class AppDev {
 
     init($httpBackend: angular.IHttpBackendService) {
         let patients: Array<Patient> = [];
+        let drugs: Array<Drug> = [];
 
+        Papa.parse('http://localhost:3474/mock/Drugs.csv', {
+            download: true,
+            header: true,
+            encoding: "UTF-8",
+            complete: function(result) {
+                console.log('parsed');
+                drugs = result.data;
+                console.log(drugs[0]);
+            }
+        });
+        
         let backendUrl = 'http://localhost:7642/RestService.svc';
 
         $httpBackend.whenGET(/\/ahv-number\/(.+)/, undefined, ['ahvNumber']).respond((method, url, data, headers, params) => {
-            console.log(params.ahvNumber);
-            let found = patients.filter((patient: Patient) => { return patient.AhvNumber === params.ahvNumber })[0];
+           let found = patients.filter((patient: Patient) => { return patient.AhvNumber === params.ahvNumber })[0];
             if (found === undefined) {
                 return [200, new Patient(null), {}];
+            } else {
+                return [200, found, {}];
+            }
+        });
+
+        $httpBackend.whenGET(/\/drugs\/search\/(.+)/, undefined, ['searchTerm']).respond((method, url, data, headers, params) => {
+           let found = drugs.filter((drug: Drug) => {
+                if (drug.DrugDescription === undefined || drug.DrugDescription === null)
+                    return false;
+                return drug.DrugDescription.indexOf(params.searchTerm) !== -1;
+            });
+            if (found === undefined) {
+                return [200, new Drug(), {}];
             } else {
                 return [200, found, {}];
             }
